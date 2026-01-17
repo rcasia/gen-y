03c6822defb1cc951fecb07770a225a9a9c58ed5
Ricardo Casía <31012661+rcasia@users.noreply.github.com>
2026-01-17 23:37:52 +0100

    chore: update Dockerfile and nginx configuration to use startup script and environment variable for port

---
+++ infrastructure/start.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+set -e
+
+# Railway proporciona PORT como variable de entorno
+# Si no está definida, usar 80 por defecto
+PORT=${PORT:-80}
+export PORT
+
+# Generar configuración de nginx con el puerto correcto
+envsubst '${PORT}' < /etc/nginx/templates/default.conf.template > /etc/nginx/conf.d/default.conf
+
+# Iniciar nginx
+exec nginx -g 'daemon off;'
